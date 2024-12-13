4e25b3866e64855e334c16a7f9ba39bf1aae4109
ievaekarklina <ievakarklina746@gmail.com>
2024-12-13 12:16:39 +0200

    Add files via upload

---
+++ PD02.py
@@ -0,0 +1,37 @@
+#2
+
+n = int(input("ievada skaitli "))
+
+summa = sum(range(1, n+1))
+print ("summas rezultāts no 1 līdz n")
+
+#3
+
+import math
+
+n = int(input("ievada skaitli "))
+print("!")  (factorial)
+
+#4
+def_ ("izvada_skaitli n")
+for i in range(n, -1): 
+    
+#5
+
+for i in range(0, augstums):
+    for j in range(0, garums):
+        
+   
+for i in range(0, garums):
+    for j in range(0, garums):
+        print('#', end=" ")
+    print()
+print()
+augstums = 5  
+for i in range(0, augstums):
+    for j in range(0, augstums * 2):
+        if i == j or j == (augstums * 2 - i - 1):
+            print('#', end="")
+        else:
+            print(' ', end="")
+    print()
